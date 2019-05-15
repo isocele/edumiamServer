1eae167f5f193549f480e4cf4d8adab57122e814
chollet <hugo.chollet@epitech.eu>
2019-05-15 12:26:59 +0200

    Hubspot inscription (vid) + update info

--- src/hubspot.js
+++ src/hubspot.js
@@ -1,4 +1,6 @@
 const requestPromise = require('request-promise');
+const json = require('../assets/countrydata.json'); //(with path)
+const parse = require('./parsingTools.js')
 
 const apikey = "2e27342c-a4bb-4b3c-a1fa-30f8b9b0f702";
 const postemail = "contact@edumiam.com";
@@ -12,10 +14,24 @@ function createProperties(req) {
     properties = [];
 
     for (let item in req.query) {
-        properties.push({
-            "property": item,
-            "value": req.query[item]
-        });
+        if (item !== "vid" && item !== "Country" && item !== "Source") {
+            properties.push({
+                "property": item,
+                "value": req.query[item]
+            });
+        }
+        else if (item === "Source") {
+            properties.push({
+                "property": "Chatfuel :" + item,
+                "value": req.query[item]
+            });
+        }
+        else if (item === "Country") {
+            properties.push({
+                "property": item,
+                "value": json[req.query[item].substring(3,5)]
+            });
+        }
     }
 }
 
@@ -25,12 +41,14 @@ module.exports = {
     hubspotRoute: async function (request, response) {
         if (!request.query.email)
             var url = 'https://api.hubapi.com/' + endpoint;
-        else
-            var url = 'https://api.hubapi.com/' + endpoint;
+        else {
+            var url = 'https://api.hubapi.com/' + endpoint + "/vid/" + request.query.vid + "/profile";
+            console.log(url);
+        }
+
         try {
             createProperties(request);
-            response.json(properties);
-            console.log(properties);
+            //    console.log(properties);
             const data = await requestPromise({
                 method: 'POST',
                 url: url,
@@ -40,11 +58,25 @@ module.exports = {
                 qs: {
                     hapikey: apikey
                 },
+                vid: request.query.vid,
                 json: true
             });
-             console.log(data.vid);
+            var setattributes = {};
+
+            if (!request.query.email) {
+                setattributes = {
+                    "set_attributes": {
+                        "vid": data.vid
+                    }
+                };
+            }
+            response.json({
+                data: properties,
+                setattributes
+            });
+            console.log(data);
         } catch (err) {
-            console.log("ya une erreur");
+            console.log(err);
         }
     }
 };
