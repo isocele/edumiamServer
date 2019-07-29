7b45206e9c78593c0f230da15fdc9c125efa563d
isocele <hugo.chollet@epitech.eu>
2019-07-29 15:29:53 +0200

    Question request connected to Google Sheet

--- src/question.js
+++ src/question.js
@@ -1,49 +1,29 @@
-var fs = require('fs');
+const GoogleSpreadsheet = require('google-spreadsheet');
+const creds = require('../client_secret.json');
+var SSI = "1cd8Ijy1j8kg5yHRLtECuXsr0JRLxB2iG8WBuyPPwYtw";
+
+function writeSheets(req, hour, day) {
+
+    const doc = new GoogleSpreadsheet(SSI);
+    // Pour plus d'info se référer : https://www.twilio.com/blog/2017/03/google-spreadsheets-and-javascriptnode-js.html
+    // Se connecte au Google Spreadsheets API.
+    let data = doc.useServiceAccountAuth(creds, function (err) {
+        // Obtiens les informations du SpreadSheets
+        doc.addRow(1, {Date: day, heure: hour, prenom: req.query.lastName, nom: req.query.firstName, question: req.query.question, chatfuelID: req.query.userID}, function (err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    });
+}
 
 
 module.exports = {
 
-
-    createQuestion: function (req, res, reqOpt) {
-        var name = req.query.firstName + ' ' + req.query.lastName;
-        var allquestion = "";
-        var file = './assets/chatbot_questions';
-
-
-        /*fs.open(file, 'r', (err, fd) => {
-            if (err) {
-                if (err.code === 'ENOENT') {
-                    console.error('myfile does not exist');
-                }
-                throw err;
-            }
-            else {
-
-            }
-        });*/
-        fs.readFile(file, 'utf8', async function (err, contents) {
-            if (contents) {
-                allquestion = contents;
-                allquestion += name + ' :' + req.query.question +'\n';
-                fs.writeFile(file, allquestion, function(err) {
-                    if(err) {
-                        res.json({
-                            status: 401,
-                            log: "File : " + file + " is not reachable"
-                        });
-                        return console.log(err);
-                    }
-                    res.json({
-                        status: 200,
-                        log: "File : " + file + " was added a new question",
-                        "messages": [{
-                            "text": "Vous venez de poser une question à notre équipe ! Un peu de patience, nous vous répondrons sous peu !"
-                        }]
-                    });
-                    console.log("The file was saved!");
-                });
-            } else if (err)
-                console.log(err);
-        });
+    createQuestion: function (req) {
+        let date = new Date();
+        let hour = date.getHours() + 'h:' + date.getMinutes();
+        let day = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+        writeSheets(req, hour, day);
     }
 };
